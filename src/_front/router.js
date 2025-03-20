0ec765287f1478e994699434ee510631da6dc2ef
biel.lil <biel1313biel@gmail.com>
2025-03-20 09:55:05 -0300

    init

---
+++ src/_front/router.js
@@ -0,0 +1,208 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import wwPage from './views/wwPage.vue';
+
+import { initializeData, initializePlugins, onPageUnload } from '@/_common/helpers/data';
+
+let router;
+const routes = [];
+
+function scrollBehavior(to) {
+    if (to.hash) {
+        return {
+            el: to.hash,
+            behavior: 'smooth',
+        };
+    } else {
+        return { top: 0 };
+    }
+}
+
+ 
+/* wwFront:start */
+import pluginsSettings from '../../plugins-settings.json';
+
+// eslint-disable-next-line no-undef
+window.wwg_designInfo = {"id":"7eb331be-840b-4de6-b0a8-9cc45f6dca79","homePageId":"7e2fb939-b1f8-4469-af24-1affeb8e0dd0","authPluginId":"1fa0dd68-5069-436c-9a7d-3b54c340f1fa","baseTag":null,"defaultTheme":"light","langs":[{"lang":"en","default":true}],"background":{},"workflows":[],"pages":[{"id":"7392a72d-7646-48fe-acc2-0ca159e40c55","linkId":"7392a72d-7646-48fe-acc2-0ca159e40c55","name":"Dashboard-cliente","folder":null,"paths":{"en":"dashboard-cliente/{{id|}}/{{menu|painel-de-controle}}-copy/{{subpage|}}","default":"dashboard-cliente/{{id|}}/{{menu|painel-de-controle}}-copy/{{subpage|}}"},"langs":["en"],"cmsDataSetPath":null,"sections":[{"uid":"7f1be03d-d5d4-4ba3-8302-fbc3685010f0","sectionTitle":"Menu Sidebar","linkId":"fb927356-46a2-41c6-a530-89f542a1adda"},{"uid":"ad7b0069-06b6-4603-84b6-ca6142e35501","sectionTitle":"Páginas","linkId":"3ce5de8f-70ff-4855-b4ed-e161c575fde6"},{"uid":"7e8c94eb-939b-406e-81b7-e557639b358e","sectionTitle":"Menu profile","linkId":"39523f3d-47ff-40ac-87b7-8d0abf9cc809"},{"uid":"fb5ac2b1-e0d9-43be-ae9a-a9dcc0f3d291","sectionTitle":"Alert","linkId":"b34d4963-6bb8-44fc-a164-85eccbb778e6"}],"pageUserGroups":[],"title":{},"meta":{"desc":{},"keywords":{},"socialDesc":{},"socialTitle":{},"structuredData":{}},"metaImage":""},{"id":"7e2fb939-b1f8-4469-af24-1affeb8e0dd0","linkId":"7e2fb939-b1f8-4469-af24-1affeb8e0dd0","name":"Home","folder":null,"paths":{"en":"home","default":"home"},"langs":["en"],"cmsDataSetPath":null,"sections":[{"uid":"59666d05-f724-4b10-8c2e-b93ddf2f6606","sectionTitle":"Login Container","linkId":"2fe2c3e5-fc28-44d1-b474-0a70555ef5c0"},{"uid":"0bc6e3a0-97f5-454b-b984-aa2140c2e87b","sectionTitle":"Alert","linkId":"e2688acd-c39c-44d0-8ecf-8390d8fbb8ca"}],"pageUserGroups":[],"title":{"en":"","fr":"Vide | Commencer à partir de zéro"},"meta":{"desc":{},"keywords":{},"socialDesc":{},"socialTitle":{},"structuredData":{}},"metaImage":""},{"id":"426830e6-b07f-463d-b566-a54dd147d4c2","linkId":"426830e6-b07f-463d-b566-a54dd147d4c2","name":"Dashboard","folder":null,"paths":{"en":"dashboard/{{menu|painel-de-controle}}/{{subpage|}}","default":"dashboard/{{menu|painel-de-controle}}/{{subpage|}}"},"langs":["en"],"cmsDataSetPath":null,"sections":[{"uid":"acab3073-63d7-4366-9465-be558f0a8621","sectionTitle":"Menu Sidebar","linkId":"a4768443-84b2-4c46-b636-1899691364d5"},{"uid":"9f5be470-927f-4763-a0f6-a5bb7684b2c3","sectionTitle":"Páginas","linkId":"11a7a180-e584-4ecc-9e0a-972fe4ccaec1"},{"uid":"dfe0f8cf-0526-4889-9a4a-831cbb47773e","sectionTitle":"Menu profile","linkId":"a508238d-1fec-4569-ac0a-763de8db29ee"},{"uid":"c515df03-9a82-488c-8953-347cb1c90975","sectionTitle":"Alert","linkId":"1d0f2694-e337-4ad0-b3d7-8400b0e97068"}],"pageUserGroups":[],"title":{},"meta":{"desc":{},"keywords":{},"socialDesc":{},"socialTitle":{},"structuredData":{}},"metaImage":""}],"plugins":[{"id":"f9ef41c3-1c53-4857-855b-f2f6a40b7186","name":"Supabase","namespace":"supabase"},{"id":"1fa0dd68-5069-436c-9a7d-3b54c340f1fa","name":"Supabase Auth","namespace":"supabaseAuth"},{"id":"9c40819b-4a8f-468f-9ba5-4b9699f3361f","name":"Charts","namespace":"chartjs"},{"id":"832d6f7a-42c3-43f1-a3ce-9a678272f811","name":"Date","namespace":"dayjs"}]};
+// eslint-disable-next-line no-undef
+window.wwg_cacheVersion = 2;
+// eslint-disable-next-line no-undef
+window.wwg_pluginsSettings = pluginsSettings;
+// eslint-disable-next-line no-undef
+window.wwg_disableManifest = false;
+
+const defaultLang = window.wwg_designInfo.langs.find(({ default: isDefault }) => isDefault) || {};
+
+const registerRoute = (page, lang, forcedPath) => {
+    const langSlug = !lang.default || lang.isDefaultPath ? `/${lang.lang}` : '';
+    let path =
+        forcedPath ||
+        (page.id === window.wwg_designInfo.homePageId ? '/' : `/${page.paths[lang.lang] || page.paths.default}`);
+
+    //Replace params
+    path = path.replace(/{{([\w]+)\|([^/]+)?}}/g, ':$1');
+
+    routes.push({
+        path: langSlug + path,
+        component: wwPage,
+        name: `page-${page.id}-${lang.lang}`,
+        meta: {
+            pageId: page.id,
+            lang,
+            isPrivate: !!page.pageUserGroups?.length,
+        },
+        async beforeEnter(to, from) {
+            if (to.name === from.name) return;
+            //Set page lang
+            wwLib.wwLang.defaultLang = defaultLang.lang;
+            wwLib.$store.dispatch('front/setLang', lang.lang);
+
+            //Init plugins
+            await initializePlugins();
+
+            //Check if private page
+            if (page.pageUserGroups?.length) {
+                // cancel navigation if no plugin
+                if (!wwLib.wwAuth.plugin) {
+                    return false;
+                }
+
+                await wwLib.wwAuth.init();
+
+                // Redirect to not sign in page if not logged
+                if (!wwLib.wwAuth.getIsAuthenticated()) {
+                    window.location.href = `${wwLib.wwPageHelper.getPagePath(
+                        wwLib.wwAuth.getUnauthenticatedPageId()
+                    )}?_source=${to.path}`;
+
+                    return null;
+                }
+
+                //Check roles are required
+                if (
+                    page.pageUserGroups.length > 1 &&
+                    !wwLib.wwAuth.matchUserGroups(page.pageUserGroups.map(({ userGroup }) => userGroup))
+                ) {
+                    window.location.href = `${wwLib.wwPageHelper.getPagePath(
+                        wwLib.wwAuth.getUnauthorizedPageId()
+                    )}?_source=${to.path}`;
+
+                    return null;
+                }
+            }
+
+            try {
+                await import(`@/pages/${page.id.split('_')[0]}.js`);
+                await wwLib.wwWebsiteData.fetchPage(page.id);
+
+                //Scroll to section or on top after page change
+                if (to.hash) {
+                    const targetElement = document.getElementById(to.hash.replace('#', ''));
+                    if (targetElement) targetElement.scrollIntoView();
+                } else {
+                    document.body.scrollTop = document.documentElement.scrollTop = 0;
+                }
+
+                return;
+            } catch (err) {
+                wwLib.$store.dispatch('front/showPageLoadProgress', false);
+
+                if (err.redirectUrl) {
+                    return { path: err.redirectUrl || '404' };
+                } else {
+                    //Any other error: go to target page using window.location
+                    window.location = to.fullPath;
+                }
+            }
+        },
+    });
+};
+
+for (const page of window.wwg_designInfo.pages) {
+    for (const lang of window.wwg_designInfo.langs) {
+        if (!page.langs.includes(lang.lang)) continue;
+        registerRoute(page, lang);
+    }
+}
+
+const page404 = window.wwg_designInfo.pages.find(page => page.paths.default === '404');
+if (page404) {
+    for (const lang of window.wwg_designInfo.langs) {
+        // Create routes /:lang/:pathMatch(.*)* etc for all langs of the 404 page
+        if (!page404.langs.includes(lang.lang)) continue;
+        registerRoute(
+            page404,
+            {
+                default: false,
+                lang: lang.lang,
+            },
+            '/:pathMatch(.*)*'
+        );
+    }
+    // Create route /:pathMatch(.*)* using default project lang
+    registerRoute(page404, { default: true, isDefaultPath: false, lang: defaultLang.lang }, '/:pathMatch(.*)*');
+} else {
+    routes.push({
+        path: '/:pathMatch(.*)*',
+        async beforeEnter() {
+            window.location.href = '/404';
+        },
+    });
+}
+
+let routerOptions = {};
+
+const isProd =
+    !window.location.host.includes(
+        '-staging.' + (process.env.WW_ENV === 'staging' ? process.env.VUE_APP_PREVIEW_URL : '')
+    ) && !window.location.host.includes(process.env.VUE_APP_PREVIEW_URL);
+
+if (isProd && window.wwg_designInfo.baseTag?.href) {
+    let baseTag = window.wwg_designInfo.baseTag.href;
+    if (!baseTag.startsWith('/')) {
+        baseTag = '/' + baseTag;
+    }
+    if (!baseTag.endsWith('/')) {
+        baseTag += '/';
+    }
+
+    routerOptions = {
+        base: baseTag,
+        history: createWebHistory(baseTag),
+        routes,
+    };
+} else {
+    routerOptions = {
+        history: createWebHistory(),
+        routes,
+    };
+}
+
+router = createRouter({
+    ...routerOptions,
+    scrollBehavior,
+});
+
+//Trigger on page unload
+let isFirstNavigation = true;
+router.beforeEach(async (to, from) => {
+    if (to.name === from.name) return;
+    if (!isFirstNavigation) await onPageUnload();
+    isFirstNavigation = false;
+    wwLib.globalVariables._navigationId++;
+    return;
+});
+
+//Init page
+router.afterEach((to, from, failure) => {
+    wwLib.$store.dispatch('front/showPageLoadProgress', false);
+    let fromPath = from.path;
+    let toPath = to.path;
+    if (!fromPath.endsWith('/')) fromPath = fromPath + '/';
+    if (!toPath.endsWith('/')) toPath = toPath + '/';
+    if (failure || (from.name && toPath === fromPath)) return;
+    initializeData(to);
+});
+/* wwFront:end */
+
+export default router;
